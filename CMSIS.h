8e250317ef8b6766212211484e929985b9c3c90c
thtfcccj <thtfcccj@qq.com>
2020-06-28 15:35:27 +0800

    现有集合提交

---
+++ CMSIS.h
@@ -0,0 +1,19 @@
+/*******************************************************************************
+
+                              CMSIS总体定义
+
+此接口主要为各MCU相关模块调用时，与MCU及体系体构解耦以实现无关化
+*******************************************************************************/
+#ifndef __CMSIS_H
+#define __CMSIS_H
+
+#ifdef SUPPORT_STM32F10
+  #include "stm32f10x.h" //含core_cm3.h
+#endif
+
+#ifdef SUPPORT_STM32F4
+  #include "stm32f4xx.h"  //含core_cm4.h
+#endif
+
+
+#endif // __CMSIS_H
